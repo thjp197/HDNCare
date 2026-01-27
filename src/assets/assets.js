317ecef526b3f53b9c6fd1526dc4dcce6d77ff15
Khong Minh Duc <kduc199@gmail.com>
2026-01-27 04:08:20 -0800

    add navbar

---
+++ src/assets/assets.js
@@ -0,0 +1,5 @@
+export const assets = {
+  logo: '/logo.png',
+  profile_pic: '/logo.png',
+  dropdown_icon: '/vite.svg',
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#5f6FFF",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
