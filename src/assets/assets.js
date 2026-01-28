ce8b9cb122904d1322bd1126add797da7cccedbe
Khong Minh Duc <kduc199@gmail.com>
2026-01-28 02:05:04 -0800

    create header

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,4 +2,7 @@ export const assets = {
   logo: '/logo.png',
   profile_pic: '/logo.png',
   dropdown_icon: '/vite.svg',
+  group_profiles: '/group_profiles.png',
+  arrow_icon: '/arrow_icon.png',
+  header_img: '/image.png',
 }
