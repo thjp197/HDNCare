860db4ebbad31c9f4718dbaa5483cd1b1269ef09
hiepnguyen <hiepnguyen@mg.com>
2026-03-02 21:44:47 +0700

    setup, connect MongoDB and cloudinary API

---
+++ backend/models/stylistModel.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const stylistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    speciality: {
+      type: String,
+      required: true,
+    },
+    degree: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      required: true,
+    },
+    fees: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+    slots_booked: {
+      type: Object,
+      default: {},
+    },
+  },
+  { minimize: false },
+);
+
+const stylistModel =
+  mongoose.models.stylist || mongoose.model("stylist", stylistSchema);
+
+export default stylistModel;
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import dns from 'dns'
+
+//Change DNS 
+dns.setServers(["1.1.1.1","8.8.8.8"]);  
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// api endpoints
+app.get('/', (req, res) => {
+  res.send('API works!')
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
