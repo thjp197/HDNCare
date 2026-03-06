c1ae3027cdcf106f49ae8f09dcf30f0a84897ae6
hiepnguyen <hiepnguyen@mg.com>
2026-03-06 12:41:45 +0700

    update write api post image (file)

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,16 +1,35 @@
-
-
-
-//API for adding stylist 
+//API for adding stylist
 const addStylist = async (req, res) => {
   try {
-    const {name, email, password, speciality, degree, experience, about, fees, address} = req.body;
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
     const imageFile = req.file;
+    res.status(200).json(imageFile);
 
-    console.log({name, email, password, speciality, degree, experience, about, fees, address}, imageFile)
-  }    catch (error) {
+    console.log(
+      {
+        name,
+        email,
+        password,
+        speciality,
+        degree,
+        experience,
+        about,
+        fees,
+        address,
+      },
+      imageFile,
+    );
+  } catch (error) {}
+};
 
-  }
-}
-
-export { addStylist }
+export { addStylist };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import { addStylist } from '../controllers/adminController.js';
-import upload from '../middlewares/multer.js';
+import express from "express";
+import { addStylist } from "../controllers/adminController.js";
+import multer from "multer";
+// import upload from '../middlewares/multer.js';
+const upload = multer({ dest: "uploads/" });
 
 const adminRouter = express.Router();
 
-adminRouter.post('/add-stylist', upload.single('image'), addStylist);
+adminRouter.post("/add-stylist", upload.single("image"), addStylist);
 
 export default adminRouter;
