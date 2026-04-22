23d645ae930301f387f78c956edef4fcb78b53ac
hiepnguyen <hiepnguyen@mg.com>
2026-04-22 16:52:09 +0700

    add Banuba client token and update CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,19 @@ const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
+// const corsOptions = {
+//   origin: true,
+//   credentials: true,
+// }
+
 const corsOptions = {
-  origin: true,
+  origin: [
+    'https://hdncare.onrender.com',
+    'https://hdncare-admin.onrender.com',
+    'http://localhost:5173' 
+  ],
   credentials: true,
 }
-
 // middlewares
 app.use(cors(corsOptions))
 app.use(express.json({ limit: '10mb' }))
